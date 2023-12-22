60d5bdbc9f0e155577bfbb84ba96286c008dc418
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-12-22 08:07:28 +0530

    home page mobile view

---
+++ scripts/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F17800",
+        dark: {
+          active: "#090B0E",
+          normal: "#546881",
+          lighter: "#B2BBC6",
+          light: {
+            hover: "#A3ADBB",
+          },
+          dark: {
+            active: "#090B0E",
+          },
+        },
+        orange: {
+          light: "#FEF2E6",
+        },
+        light: {
+          light: {
+            active: "#F0F3F6",
+          },
+        },
+      },
+      fontFamily: {
+        satoshi: ["Satoshi"],
+      },
+    },
+  },
+  plugins: [],
+};
